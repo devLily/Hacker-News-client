8fdb67c31b02d17e7975f55d1dd6102b7763dfc8
Jaekyung Bae <jkbae09@gmail.com>
2021-12-07 03:25:59 +0900

    refactor remove Dom api

--- app.js
+++ app.js
@@ -11,36 +11,23 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-// api 호출 코드의 중복
-// ajax.open("GET", NEWS_URL, false);
-// ajax.send();
-
 const newsFeed = getData(NEWS_URL);
 const ul = document.createElement("ul");
 
+// content내용 화면으로 진입 시 리스트를 삭제한다
+// appendChild 대신 문자열 사용 방법으로 ui를 구성한다
 window.addEventLister("hashchange", function () {
   const id = location.hash.substr(1);
-  // ajax.open("GET", CONTENT_URL.replace("@id", id), false);
-  // ajax.send();
-
   const newsContents = getData(CONTENT_URL.replace("@id", id));
-  const title = document.createElement("h1");
 
-  title.innerHTML = newsContents.title;
-  content.appendChild(title);
+  container.innerHTML = `
+    <h1>${newsContents.title}</h1>
+
+    <div>
+      <a href="#">목록으로</a>
+    </div>
+  `;
 });
-
-// DOM API 최소한으로 사용하기
-// for (let i = 0; i < newsFeed.length; i++) {
-//   const li = document.createElement("li");
-//   const a = document.createElement("a");
-
-//   a.href = `#${newsFeed[i].id}`;
-//   a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
-
-//   li.appendChild(a);
-//   ul.appendChild(li);
-// }
 
 for (let i = 0; i < newsFeed.length; i++) {
   const div = document.createElement("div");
