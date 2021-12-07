a3bfd063dce46ab3720941253706b1e6e0f467a8
Jaekyung Bae <jkbae09@gmail.com>
2021-12-08 00:59:41 +0900

    Implement bookmark && style

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Hacker-News-client
+
 make hacker news client by javascript and migrate to typescript
+
+## 프로젝트의 목적
+
+- JavaScript로 먼저 프로젝트를 작성해보고 TypeScript로 변환하면서 각 언어를 확실히 이해하고 활용.
+- tailwind CSS
+
+## 프로젝트의 과정
+
+- 전달되는 데이터에 따라 동적으로 화면을 구성하는 과정에서 리팩토링을 반복하며 여러 방법의 장 단점을 파악한다.
+- 템플릿 방식을 사용하는 것
+  장점: 템플릿을 사용하여 해당 ui의 명확한 구조를 확인할 수 있고 데이터가 들어갈 위치도 확인할 수 있다
+  단점 : template 안에 마킹된 데이터가 많으면 많을 수록 데이터를 replace하기 위한 반복적인 코드가 늘어난다.
+
+ex) handlebars - template library
+
+- 목적에 의해 코드를 분리하는 작업
--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 const store = {
   currentPage: 1,
+  feeds: [],
 };
 
 function getData(url) {
@@ -14,46 +15,147 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-function newsFeed() {
-  const newsFeed = getData(NEWS_URL);
-  const newsList = [];
+function checkFeeds(feeds) {
+  for (let i = 0; i < feeds.length; i++) {
+    feeds[i].read = false;
+  }
 
-  newsList.push(`<ul>`);
+  return feeds;
+}
+
+function newsFeed() {
+  let newsFeed = store.feeds;
+  const newsList = [];
+  let template = `
+  <div class="bg-gray-600 min-h-screen">
+    <div class="bg-white text-xl">
+      <div class="mx-auto px-4">
+        <div class="flex justify-between items-center py-6">
+          <div class="flex justify-start">
+            <h1 class="font-extrabold">Hacker News</h1>
+          </div>
+          <div class="items-center justify-end">
+            <a href="#/page/{{prev_page}}" class="text-gray-500">
+              Previous
+            </a>
+            <a href="#/page/{{next_page}}" class="text-gray-500 ml-4">
+              Next
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="p-4 text-2xl text-gray-700">
+    {{news_feed}}
+    </div>
+  </div>
+`;
+
+  if (!newsFeed.length) {
+    newsFeed = store.feeds = checkFeed(getData(NEWS_URL));
+    // =을 두번 사용하면 맨 오른쪽에 있는 데이터가 왼쪽에 한 번 들어가고 후에 제일 왼쪽에 들어가게 됨
+    // 같은 데이터를 연속으로 넣어줄 수 있다
+  }
 
   for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
-      <li>
-        <a href="#/show/${newsFeed[i].id}">
-          ${newsFeed[i].title} (${newsFeed[i].comments_count})
-        </a>
-      </li>
-    `);
+    <div class="p-6 ${
+      newsFeed[i].read ? "bg-green-500" : "bg-white"
+    } mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+      <div class="flex">
+        <div class="flex-auto">
+          <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>
+        </div>
+        <div class="text-center text-sm">
+          <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${
+            newsFeed[i].comments_count
+          }</div>
+        </div>
+      </div>
+      <div class="flex mt-3">
+        <div class="grid grid-cols-3 text-sm text-gray-500">
+          <div><i class="fas fa-user mr-1"></i>${newsFeed[i].user}</div>
+          <div><i class="fas fa-heart mr-1"></i>${newsFeed[i].points}</div>
+          <div><i class="far fa-clock mr-1"></i>${newsFeed[i].time_ago}</div>
+        </div>
+      </div>
+    </div>
+  `);
   }
 
-  newsList.push(`</ul>`);
-  newsList.push(`
-  <div>
-    <a href="#/page/${
-      store.currentPage > 1 ? store.currentPage - 1 : 1
-    }">이전 페이지</a>
-    <a href="#/page/${store.currentPage + 1}">다음 페이지</a>
-  </div>
-  `);
+  template = template.replace("{{news_feed}}", newsList.join(""));
+  template = template.replace(
+    "{{prev_page}}",
+    store.currentPage > 1 ? store.currentPage - 1 : 1
+  );
+  template = template.replace("{{next_page}}", store.currentPage + 1);
 
-  container.innerHTML = newsList.join("");
+  container.innerHTML = template;
 }
 
 function newsDetail() {
   const id = location.hash.substr(7);
   const newsContents = getData(CONTENT_URL.replace("@id", id));
+  let template = `
+  <div class="bg-gray-600 min-h-screen pb-8">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/${store.currentPage}" class="text-gray-500">
+                <i class="fa fa-times"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
 
-  container.innerHTML = `
-    <h1>${newsContents.title}</h1>
+      <div class="h-full border rounded-xl bg-white m-6 p-4 ">
+        <h2>${newsContents.title}</h2>
+        <div class="text-gray-400 h-20">
+          ${newsContents.content}
+        </div>
 
-    <div>
-      <a href="#/page/${store.currentPage}">목록으로</a>
+        {{__comments__}}
+
+      </div>
     </div>
   `;
+
+  for (let i = 0; i < store.feeds.length; i++) {
+    if (store.feeds[i].id === Number(id)) {
+      store.feeds[i].read = true;
+      break;
+    }
+  }
+  function displayComment(comments, count = 0) {
+    const commentList = [];
+
+    for (let i = 0; i < comments.length; i++) {
+      commentList.push(`
+      <div style="padding-left: ${count * 40}px;" class="mt-4">
+      <div class="text-gray-400">
+        <i class="fa fa-sort-up mr-2"></i>
+        <strong>${comments[i].user}</strong> ${comments[i].time_ago}
+      </div>
+      <p class="text-gray-700">${comments[i].content}</p>
+    </div>
+      `);
+
+      if (comments[i].comments.length) {
+        commentList.push(displayComment(comments[i].comments, count++));
+      }
+    }
+
+    return commentList.join("");
+  }
+  container.innerHTML = template.replace(
+    "{{__comments__}}",
+    displayComment(newsContents, comments)
+  );
 }
 
 function router() {
